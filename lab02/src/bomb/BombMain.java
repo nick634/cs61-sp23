4a3f1df30a137f140b79654429c7663820a86cda
Nick K <nick.keating.2018@gmail.com>
2023-11-13 19:32:45 -0600

    done with bomb

--- lab02/src/bomb/BombMain.java
+++ lab02/src/bomb/BombMain.java
@@ -11,13 +11,26 @@ public class BombMain {
         // TODO: Find the correct passwords to each phase using debugging techniques
         Bomb b = new Bomb();
         if (phase >= 0) {
-            b.phase0("Figure this out. I wonder where the phases are defined...");
+            b.phase0("39291226");
         }
         if (phase >= 1) {
-            b.phase1(null); // Figure this out too
+            IntList lst = IntList.of(0,9,3,0,8);
+            b.phase1(lst); // Figure this out too
         }
         if (phase >= 2) {
-            b.phase2("Figure this out. I wonder where the phases are defined...");
+            String strlst = "0";
+            int i = 1;
+            while(i < 1400){
+                if (i == 1337){
+                    strlst += " " + "-81201430";
+                    i+=1;
+                }
+                else {
+                    strlst += " " + Integer.toString(i);
+                    i += 1;
+                }
+            }
+            b.phase2(strlst);
         }
     }
 }
