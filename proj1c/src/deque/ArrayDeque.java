55f3d4a7a7cca5dd72de4a94a5a58a349a54b432
Nick K <nick.keating.2018@gmail.com>
2023-12-19 18:00:08 -0600

    copied 1a to 1c, removing length from LLD. no errors

--- proj1c/src/deque/ArrayDeque.java
+++ proj1c/src/deque/ArrayDeque.java
@@ -1,5 +1,178 @@
 package deque;
 
-public class ArrayDeque<T> implements Deque<T> {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ArrayDeque<T> implements Deque<T> {
+    T[] items;
+    int size = 0;
+    private int length = 0;
+    private int nextFirst = 0;
+    private int nextLast = 1;
+
+    private int RFACTOR = 3; //array resizing up multiple
+
+    public ArrayDeque(){
+        items = (T[]) new Object[8];
+        length = 8;
+    }
+    @Override
+    public void addFirst(T x) {
+        if (size + 1 > length){
+            items = makeBigger(); //now it's a 3x bigger array
+        }
+        size++;
+        items[nextFirst] = x;
+        if (nextFirst == 0){
+            nextFirst = length - 1;
+        }
+        else {
+            nextFirst--;
+        }
+    }
+
+    @Override
+    public void addLast(T x) {
+        if (size + 1 > length){
+            items = makeBigger();
+        }
+        size++;
+        items[nextLast] = x;
+        if (nextLast + 1 >= length){
+            nextLast = 0;
+        }
+        else {
+            nextLast++;
+        }
+    }
+
+    @Override
+    public List<T> toList() {
+        List<T> returnList = new ArrayList<>();
+        for (int i = 0; i < length; i++){
+            int trueIndex = convertIndex(i);
+            if (items[trueIndex] != null) {
+                returnList.add(i, items[trueIndex]);
+            }
+        }
+        return returnList;
+    }
+    private int convertIndex(int desiredIndex){ //converts a desired index (as the user interprets) to the array's true index
+        if (nextFirst + 1 + desiredIndex >= length){
+            return nextFirst + 1 - (length - desiredIndex);
+        }
+        return nextFirst + 1 + desiredIndex;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        if (size == 0){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+    public int length() {
+        return length;
+    }
+
+    @Override
+    public T removeFirst() {
+        if (isEmpty()){
+            return null;
+        }
+        size--;
+        if ((float) size/length < .25){
+            items = makeSmaller();
+        }
+        int firstIndex = convertIndex(0);
+        T item = items[firstIndex];
+        items[firstIndex] = null;
+        nextFirst = firstIndex;
+        return item;
+    }
+
+    @Override
+    public T removeLast() {
+        if (isEmpty()){
+            return null;
+        }
+        if ((float) (size - 1)/length < .25){
+            items = makeSmaller();
+        }
+        int lastIndex = convertIndex(size - 1); //gets index of last non-null value in list
+        T item = items[lastIndex];
+        items[lastIndex] = null;
+        nextLast = lastIndex;
+        size--;
+        return null;
+    }
+
+    @Override
+    public T get(int index) { //should i consider the last "first" index 0?
+        // nextFIrst + 1 = 0, nextFirst + 2 = 1, ...
+        if (isEmpty() || index >= length){
+            return null;
+        }
+        int trueIndex = convertIndex(index);
+        return items[trueIndex];
+    }
+    private T[] makeBigger(){
+        int firstIndex = convertIndex(0);
+        int lastIndex = convertIndex(size - 1);
+        length = length * RFACTOR;
+        T[] bigger = (T[]) new Object[length]; //so triples in size
+
+        System.arraycopy(items, firstIndex, bigger,0,size - firstIndex); //first element of list to end of memory
+        if (firstIndex != 0){
+            System.arraycopy(items, 0, bigger,size - firstIndex, lastIndex + 1); //rest of list
+        }
+        nextFirst = length - 1;
+        nextLast = size;
+        return bigger;
+    }
+    private T[] makeSmaller(){
+        if (length >= 16){
+            int newLength = length / 2;
+            T[] smaller = (T[]) new Object[newLength];
+            if (nextFirst == length - 1){ //annoying case when nextFirst is last memory box
+                System.arraycopy(items, 0, smaller,0,size + 1);
+            }
+            else{
+                System.arraycopy(items, nextFirst + 1, smaller,0,size + 1);
+            }
+            length = newLength;
+            nextFirst = length - 1;
+            nextLast = size;
+            return smaller;
+        }
+        return items;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+    public static void main(String[] args){
+        Deque<Integer> lla = new ArrayDeque<>();
+        for (int i = 0; i < 40; i++) { //[0, 1,..., 39]
+            if (i > 20){
+                lla.addLast(i);
+            }
+            else{
+                lla.addFirst(i);
+            }
+        }
+        System.out.println(lla.toList());
+        lla.removeFirst();
+        lla.removeLast();
+        System.out.println(lla.toList());
+        System.out.println(lla.get(3));
+    }
 }
+
+
--- proj1c/src/deque/LinkedListDeque.java
+++ proj1c/src/deque/LinkedListDeque.java
@@ -1,5 +1,166 @@
 package deque;
 
-public class LinkedListDeque<T> implements Deque<T> {
+import java.util.ArrayList;
+import java.util.List;
 
+public class LinkedListDeque<T> implements Deque<T> {
+    //make a deque
+    private Node sentinel;
+    int size;
+    public class Node{
+        public T item;
+        public Node next;
+        public Node previous;
+
+        private T nullify(){
+            this.next = null;
+            this.previous = null;
+            return this.item;
+        }
+    }
+    public LinkedListDeque(){
+        size = 0;
+        sentinel = new Node();
+        sentinel.next = sentinel;
+        sentinel.previous = sentinel;
+    }
+    public LinkedListDeque LinkedListDeque(LinkedListDeque other){
+        LinkedListDeque L = new LinkedListDeque<T>();
+        if (other.size() == 0){
+            return L;
+        }
+        Node n = other.sentinel.next;
+        while (n != other.sentinel){
+            Node copy = n;
+            L.addLast(n.item);
+            n = n.next;
+        }
+        return L;
+    }
+    public Node makeNode(T i){
+        Node n = new Node();
+        n.item = i;
+        return n;
+    }
+
+    public void addFirst(T item){
+        Node n = this.makeNode(item);
+        Node s = this.sentinel;
+        if (this.size == 0){ //dont love this...how else to make sentinel loop with first node?
+            s.previous = n;
+        }
+        n.next = s.next;
+        n.previous = s;
+        s.next.previous = n;
+        s.next = n;
+        size++;
+    }
+    public void addLast(T item){
+        Node n = this.makeNode(item);
+        Node s = this.sentinel;
+
+        if (this.size == 0){
+            s.next = n;
+        }
+        n.previous = s.previous;
+        n.next = s;
+        s.previous.next = n;
+        s.previous = n;
+        size++; //probably a way to make this all more elegant
+    }
+
+    public boolean isEmpty(){
+        return this.size == 0;
+    }
+    public int size(){
+        return this.size;
+    }
+    public void printDeque(){
+        Node n = this.sentinel.next;
+        while (n != this.sentinel){
+            System.out.print(n.item + " ");
+            n = n.next;
+        }
+        System.out.println();
+    }
+
+    public T removeFirst(){
+        if (this.isEmpty()){
+            return null;
+        }
+        this.size -= 1;
+        Node n = this.sentinel.next;
+        this.sentinel.next = n.next;
+        n.next.previous = sentinel;
+        T item = n.nullify();
+        return item; //should I null this or no? will garbage collector dispose of it?
+    }
+    public T removeLast(){
+        if (this.isEmpty()){
+            return null;
+        }
+        this.size -= 1;
+        Node n = this.sentinel.previous;
+        n.previous.next = sentinel;
+        sentinel.previous = n.previous;
+        T item = n.nullify();
+        return item;
+    }
+    public T get(int index){
+        if (this.isEmpty() || index >= this.size){
+            return null;
+        }
+        Node n = sentinel.next;
+        while (index > 0){
+            n = n.next;
+            index--;
+        }
+        return n.item;
+    }
+    public T getRecursive(int index){
+        if (this.size == 0 || index > this.size){
+            return null;
+        }
+        if (index == 0){
+            return this.sentinel.next.item;
+        }
+        LinkedListDeque L = this.LinkedListDeque(this); //deep copy
+        L.removeFirst();
+        return (T) L.getRecursive(index - 1);
+    }
+
+    public List<T> toList(){
+        ArrayList<T> returnList = new ArrayList<T>();
+        Node n = this.sentinel.next;
+        while (n != this.sentinel){
+            returnList.add(n.item);
+            n = n.next;
+        }
+        return returnList;
+    }
+
+    public static void main(String[] args){
+        Deque<Integer> lld = new LinkedListDeque<>();
+        LinkedListDeque L = new LinkedListDeque<String>();
+        L.sentinel.item = "69";
+        //LinkedListDeque.Node n1 = L.makeNode("hello");
+        //LinkedListDeque.Node n2 = L.makeNode("there");
+        L.addLast("hello");
+        L.addLast("there");
+        L.addLast("man");
+        LinkedListDeque L2 = L.LinkedListDeque(L);
+        L.printDeque();
+        L2.printDeque();
+        System.out.println("space");
+        L2.removeFirst();
+        L2.addFirst("hi");
+        L.printDeque();
+        L2.printDeque();
+        List<String> L3 = L2.toList();
+        System.out.println(L3);
+        System.out.println("space");
+        System.out.println(L2.getRecursive(0));
+    }
 }
+
+
